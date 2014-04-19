bea938ea4df11f5db28e6e13bb21579829a03b57
nenorbot <ronenc@gmail.com>
2014-04-19 14:37:46 +0300

    added scm

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://github.com/nenorbot/map-helpers"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :scm {:name "git"
+        :url "http://github.com/nenorbot/map-helpers"})
