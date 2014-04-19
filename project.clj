0a65fb100c1bc80bbad726459ef6427115821a8c
nenorbot <ronenc@gmail.com>
2014-04-19 13:24:24 +0300

    fixed :url in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject map-helpers "0.1.0-SNAPSHOT"
   :description "Functions for working with Clojure maps"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/nenorbot/map-helpers"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
